dd248df876e3797b30b0a877a8b1e96bb2c66d48
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-11-28 15:29:29 +0100

    Start on Seed

---
+++ Companies.API/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Companies.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Companies.API.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+
+        public static async Task SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var db = serviceProvider.GetRequiredService<APIContext>();
+
+                //db.Database.EnsureDeleted();
+                //db.Database.Migrate();
+
+                try
+                {
+                    await SeedData.InitAsync(db);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+        }
+    }
+}
