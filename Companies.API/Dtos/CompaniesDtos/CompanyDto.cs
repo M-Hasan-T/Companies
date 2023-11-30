b02180c8df327a5b0a709691516cfd135b700f18
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-11-30 19:29:51 +0100

    Queryparams

--- Companies.API/Dtos/CompaniesDtos/CompanyDto.cs
+++ Companies.API/Dtos/CompaniesDtos/CompanyDto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Companies.API.Dtos.EmployeesDtos;
+using System.ComponentModel.DataAnnotations;
 
 namespace Companies.API.Dtos.CompaniesDtos
 {
@@ -9,5 +10,6 @@ namespace Companies.API.Dtos.CompaniesDtos
         public string? Name { get; init; }
         public string? Address { get; init; }
 
+        public IEnumerable<EmployeeDto> Employees { get; init; }
     }
 }
