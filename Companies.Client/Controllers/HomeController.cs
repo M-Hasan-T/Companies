941722594e24d27f2b03b522ceb321b45bab7fec
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-12-05 16:34:45 +0100

    GetWithRequestMessageAsync

--- Companies.Client/Controllers/HomeController.cs
+++ Companies.Client/Controllers/HomeController.cs
@@ -1,32 +1,69 @@
 using Companies.Client.Models;
+using Companies.Shared.Dtos.CompaniesDtos;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace Companies.Client.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly HttpClient httpClient;
+        private const string json = "application/json";
+        public HomeController()
         {
-            _logger = logger;
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:7259");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var res = await SimpleGetAsync();
+            var res2 = await SimpleGetAsync2();
+            var res3 = await GetWithRequestMessageAsync();
+            var res4 = await PostWithRequestMessageAsync();
+
             return View();
         }
 
-        public IActionResult Privacy()
+        private async Task<CompanyDto> PostWithRequestMessageAsync()
         {
-            return View();
+            throw new NotImplementedException();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        private async Task<IEnumerable<CompanyDto>> GetWithRequestMessageAsync()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/companies");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
+
+            var response = await httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var res = await response.Content.ReadAsStringAsync();
+            var companies = JsonSerializer.Deserialize<IEnumerable<CompanyDto>>(res,
+                new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            return companies!;
+        }
+
+        private async Task<IEnumerable<CompanyDto>> SimpleGetAsync()
+        {
+            var response = await httpClient.GetAsync("api/companies");
+            response.EnsureSuccessStatusCode();
+
+            var res = await response.Content.ReadAsStringAsync();
+            var companies = JsonSerializer.Deserialize<IEnumerable<CompanyDto>>(res,
+                new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            return companies!;
+        }
+
+        private async Task<IEnumerable<CompanyDto>?> SimpleGetAsync2()
+        {
+            return await httpClient.GetFromJsonAsync<IEnumerable<CompanyDto>>("api/companies",
+                new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
         }
     }
 }
