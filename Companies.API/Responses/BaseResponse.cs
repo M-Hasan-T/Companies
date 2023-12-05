413059209a890ecaa1c8be71b1a3c6ff4f939c6f
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-12-05 14:06:45 +0100

    Custom responses start!

---
+++ Companies.API/Responses/BaseResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Companies.API.Responses
+{
+    //public abstract class BaseResponse
+    //{
+    //    public bool Success { get; }
+    //    protected BaseResponse(bool success) => Success = success;
+    //}
+    public abstract record BaseResponse(bool Success);
+
+    //public sealed class OkResponse<T> : BaseResponse
+    //{
+    //    public T Result { get; }
+    //    public OkResponse(T Result) : base(true) => Result = result;
+    //}
+    public sealed record OkResponse<T>(T Result) : BaseResponse(true);
+
+    public abstract record NotFoundResponse(string Message) : BaseResponse(false);
+    public record CompanyNotFoundResponse(Guid Id) : NotFoundResponse($"The company with id: {Id} doesn't exist");
+}
