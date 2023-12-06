5cb3c0e904411d3545e2cb93d634d4cbfec7dc7c
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-12-06 23:31:04 +0100

    CompaniesClient

---
+++ Companies.Client/Clients/CompaniesClient.cs
@@ -0,0 +1,35 @@
+﻿using Companies.Shared.Dtos.CompaniesDtos;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace Companies.Client.Clients
+{
+    public class CompaniesClient : ICompaniesClient
+    {
+        private readonly HttpClient client;
+        private const string json = "application/json";
+        public CompaniesClient(HttpClient httpClient)
+        {
+            this.client = httpClient;
+            client.BaseAddress = new Uri("https://localhost:7259");
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
+            client.Timeout = new TimeSpan(0, 0, 5);
+        }
+
+        public async Task<T?> GetAsync<T>(string path, string contentType = json)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, path);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(contentType));
+
+            var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            response.EnsureSuccessStatusCode();
+
+            var stream = await response.Content.ReadAsStreamAsync();
+            var result = JsonSerializer.Deserialize<T>(stream,
+                new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            return result;
+        }
+    }
+}
--- Companies.Client/Controllers/HomeController.cs
+++ Companies.Client/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using Companies.Client.Clients;
+using Companies.Client.Helpers;
 using Companies.Client.Models;
 using Companies.Shared.Dtos.CompaniesDtos;
 using Companies.Shared.Dtos.EmployeesDtos;
@@ -13,11 +15,13 @@ namespace Companies.Client.Controllers
     public class HomeController : Controller
     {
         private readonly HttpClient httpClient;
+        private readonly ICompaniesClient companiesClient;
         private const string json = "application/json";
-        public HomeController(IHttpClientFactory httpClientFactory)
+        public HomeController(IHttpClientFactory httpClientFactory, ICompaniesClient companiesClient)
         {
-            httpClient = httpClientFactory.CreateClient();
-            httpClient.BaseAddress = new Uri("https://localhost:7259");
+            httpClient = httpClientFactory.CreateClient("CompaniesClient");
+            this.companiesClient = companiesClient;
+            //httpClient.BaseAddress = new Uri("https://localhost:7259");
         }
 
         public async Task<IActionResult> Index()
@@ -26,7 +30,13 @@ namespace Companies.Client.Controllers
             //var res2 = await SimpleGetAsync2();
             //var res3 = await GetWithRequestMessageAsync();
             //var res4 = await PostWithRequestMessageAsync();
-            await PatchWithRequestMessageAsync();
+            //await PatchWithRequestMessageAsync();
+
+            var res1 = await companiesClient.GetAsync<IEnumerable<CompanyDto>>(UriHelpers.GetCompany(includeEmployees: true));
+            var res2 = await companiesClient.GetAsync<IEnumerable<CompanyDto>>(UriHelpers.GetCompany());
+            var res3 = await companiesClient.GetAsync<CompanyDto>(UriHelpers.GetCompany("eeca250d-60a5-409c-d46f-08dbf6a69b5a"));
+            var res4 = await companiesClient.GetAsync<IEnumerable<EmployeeDto>>(UriHelpers.GetEmployeesForCompany("eeca250d-60a5-409c-d46f-08dbf6a69b5a"));
+
 
             return View();
         }
@@ -39,9 +49,9 @@ namespace Companies.Client.Controllers
 
             var serializedPatchDoc = Newtonsoft.Json.JsonConvert.SerializeObject(patchDocument);
 
-            var request = new HttpRequestMessage(HttpMethod.Patch, "api/companies/96bbaba3-2503-4973-a4eb-08dbf5e33284/employees/f0327c87-2d2e-4dd4-ac88-08dbf5e33286");
+            var request = new HttpRequestMessage(HttpMethod.Patch, "api/companies/eeca250d-60a5-409c-d46f-08dbf6a69b5a/employees/d27c4a09-b30d-4515-d543-08dbf6a69b5c");
             request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
-            request.Content.Headers.ContentType = new MediaTypeHeaderValue(json);
+            //request.Content.Headers.ContentType = new MediaTypeHeaderValue(json);
             request.Content = new StringContent(serializedPatchDoc);
 
             var response = await httpClient.SendAsync(request);
@@ -51,7 +61,7 @@ namespace Companies.Client.Controllers
         private async Task<CompanyDto> PostWithRequestMessageAsync()
         {
             var request = new HttpRequestMessage(HttpMethod.Post, "api/companies");
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
+            //request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
 
             var companyToCreate = new CompanyForCreationDto
             {
@@ -75,19 +85,9 @@ namespace Companies.Client.Controllers
             return companyDto!;
         }
 
-        private async Task<IEnumerable<CompanyDto>> GetWithRequestMessageAsync()
+        private async Task<IEnumerable<CompanyDto>?> GetWithRequestMessageAsync()
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, "api/companies");
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
-
-            var response = await httpClient.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-
-            var res = await response.Content.ReadAsStringAsync();
-            var companies = JsonSerializer.Deserialize<IEnumerable<CompanyDto>>(res,
-                new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
-
-            return companies!;
+            return await companiesClient.GetAsync<IEnumerable<CompanyDto>>("api/companies");
         }
 
         private async Task<IEnumerable<CompanyDto>> SimpleGetAsync()
