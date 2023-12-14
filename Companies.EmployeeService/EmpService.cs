8d31ad5868ea497b830d5b1a3536b23ef38d2776
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-12-14 22:53:01 +0100

    EmpService

---
+++ Companies.EmployeeService/EmpService.cs
@@ -0,0 +1,40 @@
+﻿namespace Companies.EmployeeService
+{
+    public class EmpService
+    {
+        private readonly IValidator validator;
+
+        public EmpService(IValidator validator)
+        {
+            this.validator = validator;
+        }
+
+        public bool RegisterUser(Employee employee)
+        {
+            //Do a lot of stuff..
+            return validator.ValidateName(employee);
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Salary { get; set; }
+
+        public SalaryLevel salaryLevel { get; set; }
+    }
+
+    public enum SalaryLevel
+    {
+        Default,
+        NotSet,
+        Junior,
+        Senior
+    }
+
+    public interface IValidator
+    {
+        SalaryLevel ValidateSalaryLevel(Employee employee);
+        bool ValidateName(Employee employee);
+    }
+}
