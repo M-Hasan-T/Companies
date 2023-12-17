921a7dec0043c3aa08697e8d2f5846588b41ba39
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-12-17 18:28:24 +0100

    First Integration Test

---
+++ Companies.Integration.Tests/DemoControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Companies.API;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace Companies.Integration.Tests
+{
+    public class DemoControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private HttpClient httpClient;
+
+        public DemoControllerTests(WebApplicationFactory<Program> applicationFactory)
+        {
+            applicationFactory.ClientOptions.BaseAddress = new Uri("https://localhost:7259/api/demo/");
+            httpClient = applicationFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Index_ShouldReturnOk()
+        {
+            var response = await httpClient.GetAsync("");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
